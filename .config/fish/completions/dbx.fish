06a86900d0737cf0f1daa2f0792797335d49cbbb
Judah Rand <17158624+judahrand@users.noreply.github.com>
2022-12-14 10:56:51 +0000

    Add `dbx` completions

---
+++ .config/fish/completions/dbx.fish
@@ -0,0 +1,1 @@
+complete --command dbx --no-files --arguments "(env _DBX_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) dbx)" --condition "env _DBX_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) dbx"
